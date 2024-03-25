d8e4354335e221ed2f981c3c7cedd65d32755ceb
minsiter02 <A20213133@HackerLogin.8.8.8.8>
2024-03-25 20:34:10 +0900

    solve 2557

---
+++ a.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("int 자료형의 크기: %d바이트\n", sizeof(int));
+    printf("float 자료형의 크기: %d바이트\n", sizeof(float));
+    printf("char 자료형의 크기: %d바이트\n", sizeof(char));
+    printf("double 자료형의 크기: %d바이트\n", sizeof(double));
+    printf("long 자료형의 크기: %d바이트\n", sizeof(long));
+    printf("long long int 자료형의 크기: %d바이트\n", sizeof(long long int));
+    printf("unsigned int 자료형의 크기: %d바이트\n", sizeof(unsigned));
+}
